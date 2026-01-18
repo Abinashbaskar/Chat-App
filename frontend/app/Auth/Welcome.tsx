3296ff4f6b0e7a5df384852ab8b7397f40d5941c
Abinashbaskar <babinash007@gmail.com>
2026-01-18 14:49:39 +0530

    Welcome Page

--- frontend/app/Auth/Welcome.tsx
+++ frontend/app/Auth/Welcome.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const Welcome = () => {
-    return (
-        <div>Welcome</div>
-    )
-}
+    const router = useRouter();
 
-export default Welcome
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Welcome</Text>
+            <TouchableOpacity onPress={() => router.push('/ChatList')} style={styles.button}>
+                <Text style={styles.buttonText}>Go to Chats</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+export default Welcome;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+        fontWeight: 'bold',
+    },
+    button: {
+        padding: 15,
+        backgroundColor: '#007AFF',
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+    },
+});
